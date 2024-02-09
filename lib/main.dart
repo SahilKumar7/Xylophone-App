9fccb348bff0651f1506aa0be477094a00138792
Sahil <sahil.kumar5729@gmail.com>
2024-02-09 15:40:58 +0530

    Changed button shapes

--- lib/main.dart
+++ lib/main.dart
@@ -12,104 +12,140 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FilledButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(
-                    AssetSource('note1.wav'),
-                  );
-                },
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.redAccent),
-                  enableFeedback: false,
+              Expanded(
+                child: FilledButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource('note1.wav'),
+                    );
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.redAccent),
+                    enableFeedback: false,
+                    shape: MaterialStatePropertyAll<OutlinedBorder>(
+                      BeveledRectangleBorder(),
+                    ),
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
-              FilledButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(
-                    AssetSource('note2.wav'),
-                  );
-                },
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.orangeAccent),
-                  enableFeedback: false,
+              Expanded(
+                child: FilledButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource('note2.wav'),
+                    );
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.orangeAccent),
+                    enableFeedback: false,
+                    shape: MaterialStatePropertyAll<OutlinedBorder>(
+                      BeveledRectangleBorder(),
+                    ),
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
-              FilledButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(
-                    AssetSource('note3.wav'),
-                  );
-                },
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.yellowAccent),
-                  enableFeedback: false,
+              Expanded(
+                child: FilledButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource('note3.wav'),
+                    );
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.yellowAccent),
+                    enableFeedback: false,
+                    shape: MaterialStatePropertyAll<OutlinedBorder>(
+                      BeveledRectangleBorder(),
+                    ),
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
-              FilledButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(
-                    AssetSource('note4.wav'),
-                  );
-                },
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.lightGreenAccent),
-                  enableFeedback: false,
+              Expanded(
+                child: FilledButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource('note4.wav'),
+                    );
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Colors.lightGreenAccent),
+                    enableFeedback: false,
+                    shape: MaterialStatePropertyAll<OutlinedBorder>(
+                      BeveledRectangleBorder(),
+                    ),
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
-              FilledButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(
-                    AssetSource('note5.wav'),
-                  );
-                },
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.greenAccent),
-                  enableFeedback: false,
+              Expanded(
+                child: FilledButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource('note5.wav'),
+                    );
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.greenAccent),
+                    enableFeedback: false,
+                    shape: MaterialStatePropertyAll<OutlinedBorder>(
+                      BeveledRectangleBorder(),
+                    ),
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
-              FilledButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(
-                    AssetSource('note6.wav'),
-                  );
-                },
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.blueAccent),
-                  enableFeedback: false,
+              Expanded(
+                child: FilledButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource('note6.wav'),
+                    );
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.blueAccent),
+                    enableFeedback: false,
+                    shape: MaterialStatePropertyAll<OutlinedBorder>(
+                      BeveledRectangleBorder(),
+                    ),
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
-              FilledButton(
-                onPressed: () async {
-                  final player = AudioPlayer();
-                  await player.play(
-                    AssetSource('note7.wav'),
-                  );
-                },
-                style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Colors.deepPurpleAccent),
-                  enableFeedback: false,
+              Expanded(
+                child: FilledButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(
+                      AssetSource('note7.wav'),
+                    );
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Colors.deepPurpleAccent),
+                    enableFeedback: false,
+                    shape: MaterialStatePropertyAll<OutlinedBorder>(
+                      BeveledRectangleBorder(),
+                    ),
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
               ),
             ],
           ),
